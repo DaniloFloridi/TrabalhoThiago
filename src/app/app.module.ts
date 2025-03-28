3ab98c78cdd3515add125fd7bc4e1cce0f2c65ca
DaniloFloridi <danilofloridiseriado@gmail.com>
2025-03-27 23:16:40 -0300

    Commit Att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,35 @@ import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import { TableComponent } from './components/table/table.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    TableComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatTableModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBar,
+    MatSortModule,
+    MatPaginatorModule
 
   ],
   providers: [],
